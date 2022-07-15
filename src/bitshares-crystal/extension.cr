7b74491bebef5fe3aa6545ffa788b7e3f9f0fe1d
syalon <empty@email.com>
2022-07-15 15:24:38 +0800

    ext for json any

--- src/bitshares-crystal/extension.cr
+++ src/bitshares-crystal/extension.cr
@@ -29,6 +29,19 @@ class Exception
 end
 
 struct JSON::Any
+  def is_true? : Bool
+    case v = @raw
+    when Int
+      return v != 0
+    when String
+      return v.downcase == "true"
+    when Bool
+      return v
+    else
+      return false
+    end
+  end
+
   def to_i32 : Int32
     return as_s.to_i32 if @raw.is_a?(String)
     return as_i
